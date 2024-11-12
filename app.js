1d4f1efda488344724dd4c2d6dcaf03bad516851
박건우 <woorl201362@gmail.com>
2024-11-12 14:38:16 +0900

    리팩토링
    리팩토링

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 import express from 'express'
-
 import tweetsRouter from './router/tweets.js'
-
 import authRouter from './router/auth.js'
 
 const app = express()
@@ -9,8 +7,8 @@ const app = express()
 app.use(express.json())
 
 app.use('/tweets', tweetsRouter)
-
 app.use('/auth', authRouter)
+
 
 app.use((req, res, next) => {
     res.sendStatus(404)
--- router/auth.js
+++ router/auth.js
@@ -1,14 +1,16 @@
 import express from 'express'
 
-const router = express.Router() // 라우터 기능 사용
+const router = express.Router()
+
 
 // 회원가입
 
+
 // 로그인
+
 
 // 로그인 유지
 
 
 
-
-export default router // 객체 이름으로 설정. 외부에서 사용 가능
+export default router

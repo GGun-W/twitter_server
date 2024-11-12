11403d3c7bf977dfa04281daeb9db2a2c54f2f0d
박건우 <woorl201362@gmail.com>
2024-11-12 11:54:28 +0900

    API 테스트
    API 테스트

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 import express from 'express'
-import tweetsRouter from './router/tweets.js' // 연결
-import authRouter from './router/auth.js' // 연결
 
-const app = express() // 객체 생성
+import tweetsRouter from './router/tweets.js'
 
-app.use(express.json()) // 미들웨어 생성. json사용
+import authRouter from './router/auth.js'
+
+const app = express()
+
+app.use(express.json())
 
 app.use('/tweets', tweetsRouter)
+
 app.use('/auth', authRouter)
 
-
-app.use((req, res, next) => { // 라우팅 사용
+app.use((req, res, next) => {
     res.sendStatus(404)
 })
 
-app.listen(8080) // 대기 설정. 8080->라우터 등 기본 포트 3000번이 존재하기 때문에. 충돌 방지
+app.listen(8080)
